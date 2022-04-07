b7c9aeea7e969018ff59fc38e093003543c9d50d
Katie Kirby <jackalhandz@protonmail.com>
2022-04-07 09:27:19 -0700

    Transaction template file
    .js file

---
+++ transaction.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3')
+const rpcURL = 'http://127.0.0.1:7545'
+const web3 = new Web3(rpcURL)
+
+const account1 = "0x043..."
+const account2 = "0x087..."
+
+web3.eth.sendTransaction({
+  from: account1,
+  to: account2,
+  value: web3.utils.toWei('1', 'ether')
+},
+(err, result)
+ => {
+   console.log(result)
+ }
+})
